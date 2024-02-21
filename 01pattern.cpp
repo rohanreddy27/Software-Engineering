ae2f7cb7ac980a5db95ae90d24f2d18d36487d00
rohanreddy27 <137768496+rohanreddy27@users.noreply.github.com>
2024-02-21 20:49:42 +0530

    Add files via upload

---
+++ 01pattern.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+int n;
+
+cin>>n;
+
+int count;
+for(int i=1; i<=n; i++)
+{
+    for(int j=1; j<=i; j++)
+    {
+        if((i+j)%2==0)
+        {
+            count=1;
+        }
+        else
+        {
+            count=0;
+        }
+        cout<<count;
+    }
+    cout<<endl;
+}
+
+
+
+
+}
